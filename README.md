87133c4ae8f8440847e83822dbaa1defe537a305
Praise Okhae <praiseomozoba@gmail.com>
2022-08-30 17:06:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## MY-PRAISE app
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+   
+    <div className='App' >
+
+      <h1 className='h1'>hhhhh</h1>
+      <p className='first'>jsdldlnolnojdgjdOhdohivhnDI</p>
+      <p>jsdldlnolnojdgjdOhdohivhnDI</p>
+      <p>jsdldlnolnojdgjdOhdohivhnDI</p>
+
+
+
+    </div>
+  );
+}
+
+export default App;
